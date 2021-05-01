7201c16a64350f41040eef5a83269866355f7002
BadaHertz52 <lmlwpeace@naver.com>
2021-05-01 17:20:58 +0900

    Make todolist

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import  React from 'react';
+import {createGlobalStyle} from 'styled-components';
+import TodoCreate from './components/TodoCreate';
+import TodoHead from './components/TodoHead';
+import TodoList from './components/TodoList';
+// styled-componets 는 자동적으로 지역적으로 기능하지만 createGlobalStyle을 설정하면 전역적으로 작동 가능 
+import TodoTemplate from './components/TodoTemplate';
+import { TodoProvider } from './TodoContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const GlobalStyle = createGlobalStyle`
+  body{
+    background:#e9ecef;
+  }
+`; 
 
+function App(){
+  return(
+    <TodoProvider>
+      <GlobalStyle/>
+      <TodoTemplate>
+        <TodoHead/>
+        <TodoList/>
+        <TodoCreate/>
+      </TodoTemplate>
+    </TodoProvider>
+  )
+};
 export default App;
